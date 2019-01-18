4c83b108c7afa88781e658888e6a803c156f83b9
LelandYan <2721599586@qq.com>
2019-01-18 09:33:26 +0800

    pandas_learning

--- Chapter_01/Ex_01.py
+++ Chapter_01/Ex_01.py
@@ -41,10 +41,10 @@ def top_counts(count_dict,n=10):
     value_key_pairs.sort()
     return value_key_pairs[-n:]
 
-print(top_counts(counts))
+# print(top_counts(counts))
 
-from collections import Counter
-counts = Counter(time_zones)
+# from collections import Counter
+# counts = Counter(time_zones)
 #print(counts.most_common(10))
 
 
@@ -61,6 +61,36 @@ tz_counts = clean_tz.value_counts()
 # print(tz_counts[:10])
 
 
-print(tz_counts[:10])
-data = np.random.randint(10,100,size=10)
-print(data)
+# tz_counts[:10].plot(kind='barh',rot=0)
+# plt.show()
+# times = [counts[0] for counts in tz_counts]
+# site = [counts[1] for counts in tz_counts]
+
+result = Series([x.split()[0] for x in frame.a.dropna()])
+#print(result.value_counts()[:8])
+cframe = frame[frame.a.notnull()]
+operaing_system = np.where(cframe['a'].str.contains('Windows'),'Windows','Not Windows')
+# print(operaing_system[:5])
+by_tz_os = cframe.groupby(['tz',operaing_system])
+agg_counts = by_tz_os.size().unstack().fillna(0)
+#print(by_tz_os.size().unstack())
+#print(agg_counts[:10])
+indexer = agg_counts.sum(1).argsort()
+count_subset = agg_counts.take(indexer)[-10:]
+# print(count_subset)
+count_subset.plot(kind='barh',stacked=True)
+# plt.show()
+normed_subset = count_subset.div(count_subset.sum(1),axis=0)
+normed_subset.plot(kind='barh',stacked=True)
+# plt.show()
+
+
+unames = ['user_id','gender','age','occupation','zip']
+users = pd.read_table('users.dat',sep='::',header=None,names=unames,engine='python')
+rnames = ['user_id','movie_id','rating','timestamp']
+ratings = pd.read_table('ratings.dat',sep="::",header=None,names=rnames,engine='python')
+mnames = ['movie_id','title','genres']
+movies = pd.read_table('movies.dat',sep="::",header=None,names=mnames,engine='python')
+print(users[:5])
+
+
