624c8a7d77f039bbd4d4f9578540a92f05f5c7e3
LelandYan <2721599586@qq.com>
2019-01-21 16:05:53 +0800

    +

---
+++ Chapter_04/svm_learning.py
@@ -0,0 +1,9 @@
+# _*_ coding: utf-8 _*_
+__author__ = 'LelandYan'
+__date__ = '2019/1/20 21:29'
+
+import numpy as np
+from sklearn.svm import SVC
+
+X = np.array([[-1, -1], [-2, -1], [1, 1], [2, 1]])
+y = np.array([1, 1, 2, 2])
